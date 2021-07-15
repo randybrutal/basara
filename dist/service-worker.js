533b0a0cced151a7382da2732093cc624bf240f4
Randy_Lin <randy_lin@mail.chungyo.net>
2021-07-15 17:43:05 +0800

    add pwa

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,3 +1,3 @@
-importScripts("/precache-manifest.1b0c70ebb1a23009522e29774597d111.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.9712019acee7d99e5cdceb0c2a7a5ad6.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 
